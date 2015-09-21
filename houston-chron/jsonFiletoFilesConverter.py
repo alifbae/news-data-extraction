d3300ee489d0c40725bbc8abde2ceec83ef88151
Ali Fawad <m.alifawad@gmail.com>
2015-09-21 23:48:02 +0500

    Houston Chronicle Cleaned Data Scraped

---
+++ houston-chron/jsonFiletoFilesConverter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import json
+import os
+
+
+
+def makeSingleJson():
+	dirPath = "articleData/"
+	paths = os.listdir(dirPath)
+
+	for filePath in paths:
+		print "articleData2/"+filePath
+		with open(dirPath+filePath) as f:
+			articleData = json.load(f)
+			with open("latimes-data-all.json", 'a+') as g:
+				g.write(json.dumps(articleData)+'\n')
+	return
+
+def makeJsonFiles():
+	articleData = []
+	articleCounter = 0
+
+	with open("houstonChron-data-all.json") as f:
+		for line in f:
+			articleData.append(json.loads(line))
+
+	for data in articleData:
+		with open(("articleData2/houstonChron-article-"+str(articleCounter)+".json"), 'w+') as g:
+			g.write(json.dumps(data))
+		articleCounter += 1
+	return
+
+# makeSingleJson()
+
+makeJsonFiles()
